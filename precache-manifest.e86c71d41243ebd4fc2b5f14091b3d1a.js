f83ccb768e0880d7c23704fa3e8a3dd88dfbff15
Diego Chagas <drochachagas@gmail.com>
2020-10-14 10:38:35 -0300

    Updates

---
+++ precache-manifest.e86c71d41243ebd4fc2b5f14091b3d1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e4d095bf3237a17e6e1f6fdd420eee",
+    "url": "/pokemon-list/index.html"
+  },
+  {
+    "revision": "8df6cdb80fe17eebfc28",
+    "url": "/pokemon-list/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "8df6cdb80fe17eebfc28",
+    "url": "/pokemon-list/static/js/2.0141f6a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokemon-list/static/js/2.0141f6a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b17853ebb4f2ef16a3a1",
+    "url": "/pokemon-list/static/js/main.71d826e8.chunk.js"
+  },
+  {
+    "revision": "8d51e808464e47129793",
+    "url": "/pokemon-list/static/js/runtime-main.961a3291.js"
+  }
+]);
